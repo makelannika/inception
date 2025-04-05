f14f1d57974bbf3bfacd63cba11dca57a14d9ee3
makelannika <makelannika@gmail.com>
2025-04-05 20:17:55 +0300

    wordpress entrypoint script

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('WORDPRESS_DB_NAME', getenv('WORDPRESS_DB_NAME'));
-define('WORDPRESS_DB_USER', getenv('WORDPRESS_DB_USER'));
-define('WORDPRESS_DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
-define('WORDPRESS_DB_HOST', getenv('WORDPRESS_DB_HOST'));
+define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
+define('DB_USER', getenv('WORDPRESS_DB_USER'));
+define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
+define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
